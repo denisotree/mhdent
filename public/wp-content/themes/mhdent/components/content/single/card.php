5eb53274491125b32a8ae3a2b96bf61bee490def
denisotree <denisotree@gmail.com>
2019-06-22 14:52:44 +0300

    Сделал футер

--- public/wp-content/themes/mhdent/components/content/single/card.php
+++ public/wp-content/themes/mhdent/components/content/single/card.php
@@ -9,18 +9,20 @@ $permalink = get_the_permalink();
 
 <div class="card__wrapper">
     <a href="<?= $permalink; ?>">
-        <?php if (has_post_thumbnail()) { ?>
-            <div class="card__thumbnail">
+        <div class="card__box">
+            <?php if (has_post_thumbnail()) { ?>
+                <div class="card__thumbnail">
                 <img src="<?= $thumbnail ?>" alt="<?= $title; ?>">
             </div>
-        <?php } ?>
-        <div class="card__description">
+            <?php } ?>
+            <div class="card__description">
             <div class="card__title">
                 <?= $title; ?>
             </div>
             <div class="card__excerpt">
                 <?= $excerpt; ?>
             </div>
+        </div>
         </div>
     </a>
 </div>
--- public/wp-content/themes/mhdent/footer.php
+++ public/wp-content/themes/mhdent/footer.php
@@ -5,13 +5,44 @@
 $footer_email = get_option('contact_email');
 $footer_facebook = get_option('facebook');
 $footer_vk = get_option('vk');
-$copyright_ru = get_option('copyright_ru');
-$copyright_en = get_option('copyright_en');
+
+if (pll_current_language() == 'ru') {
+    $copyright = get_option('copyright_ru');
+} elseif (pll_current_language() == 'en') {
+    $copyright = get_option('copyright_en');
+} else {
+    $copyright = get_option('copyright_en');
+}
 
 ?>
 
 <footer>
-
+    <div class="footer__container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="footer__social">
+                    <?php pll_e('Мы в социальных сетях'); ?>
+                    <div class="footer__social-links">
+                        <a class="footer__social-item" href="<?= $footer_facebook ?>"><i class="fab fa-facebook-f"></i></a>
+                        <a class="footer__social-item" href="<?= $footer_vk ?>"><i class="fab fa-vk"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="footer__anagram">
+                    <img src="<?= get_template_directory_uri() ?>/images/anagram.png" alt="">
+                </div>
+                <div class="footer__copyright">
+                    <?= $copyright ?>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="footer__contacts">
+                    <?= 'E-mail:&nbsp;<a href="mailto:' . $footer_email . '">' . $footer_email . '</a>'?>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
