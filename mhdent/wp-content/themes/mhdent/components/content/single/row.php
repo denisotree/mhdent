be6696d0aa48fc368c40a2765bc3c22eca8f0884
denisotree <denisotree@gmail.com>
2019-06-25 13:07:20 +0300

    Сделал красивые новости

--- mhdent/wp-content/themes/mhdent/components/content/single/row.php
+++ mhdent/wp-content/themes/mhdent/components/content/single/row.php
@@ -1,18 +1,30 @@
 <?php
 
 $title = get_the_title();
+$excerpt = get_the_excerpt();
+$thumbnail = get_the_post_thumbnail_url();
 $permalink = get_the_permalink();
+$date = get_the_date('d/m/Y')
 
 ?>
 
 <div class="card__wrapper">
     <a href="<?= $permalink; ?>">
-        <div class="card__box">
-            <div class="card__description">
-            <div class="card__title">
-                <?= $title; ?>
+        <div class="card__row-box">
+            <div class="card__row-thumbnail">
+                <img src="<?= $thumbnail ?>" alt="<?= $title ?>">
             </div>
-        </div>
+            <div class="card__row-description">
+                <div class="card__row-date">
+                    <?= $date ?>
+                </div>
+                <div class="card__row-title">
+                    <?= $title; ?>
+                </div>
+                <div class="card__row-excerpt">
+                    <?= $excerpt; ?>
+                </div>
+            </div>
         </div>
     </a>
 </div>
